c96c30844c8bf57d0db7fda2af9ab6ce64d27be0
Horia Botezatu <horia12btz@gmail.com>
2022-05-12 22:15:08 +0300

    Add project files.

---
+++ Space/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿
+namespace Space
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Up = new System.Windows.Forms.Timer(this.components);
+            this.Down = new System.Windows.Forms.Timer(this.components);
+            this.Left = new System.Windows.Forms.Timer(this.components);
+            this.Right = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.pictureBox1.Image = global::Space.Properties.Resources.normal;
+            this.pictureBox1.Location = new System.Drawing.Point(960, 540);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Up
+            // 
+            this.Up.Interval = 1;
+            this.Up.Tick += new System.EventHandler(this.Up_Tick);
+            // 
+            // Down
+            // 
+            this.Down.Interval = 1;
+            this.Down.Tick += new System.EventHandler(this.Down_Tick);
+            // 
+            // Left
+            // 
+            this.Left.Interval = 1;
+            this.Left.Tick += new System.EventHandler(this.Left_Tick);
+            // 
+            // Right
+            // 
+            this.Right.Interval = 1;
+            this.Right.Tick += new System.EventHandler(this.Right_Tick);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pictureBox2.Location = new System.Drawing.Point(786, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(224, 102);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Visible = false;
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(1652, 888);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(192, 97);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Space.Properties.Resources.CameraSuS;
+            this.ClientSize = new System.Drawing.Size(1904, 1041);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer Up;
+        private System.Windows.Forms.Timer Down;
+        private System.Windows.Forms.Timer Left;
+        private System.Windows.Forms.Timer Right;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Space/Form1.cs
@@ -0,0 +1,291 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Space
+{
+    public partial class Form1 : Form
+    {
+
+        public int newX = 0;
+        public int newY = 0;
+        public const int pxlmvX = 15;
+        public const int pxlmvY = 15;
+
+        public bool roomUp = false;
+        public bool roomDown = false;
+        public bool roomLeft = false;
+        public bool roomRight = false;
+        public bool inRoom = false;
+
+        string[] pictureBoxArray = new string[] { @"C:\Users\One\source\repos\Space\Space\Resources\normal.png", @"C:\Users\One\source\repos\Space\Space\Resources\normalLeft.png", @"C:\Users\One\source\repos\Space\Space\Resources\back.png", @"C:\Users\One\source\repos\Space\Space\Resources\down.png" };
+        Image hub = new Bitmap(@"C:\Users\One\source\repos\Space\Space\Resources\Hub.png");
+        Image rmup = new Bitmap(@"C:\Users\One\source\repos\Space\Space\Resources\CameraSus.png");
+        Image rmdown = new Bitmap(@"C:\Users\One\source\repos\Space\Space\Resources\CameraJos.png");
+        Image rmleft = new Bitmap(@"C:\Users\One\source\repos\Space\Space\Resources\CameraStanga.png");
+        Image rmright = new Bitmap(@"C:\Users\One\source\repos\Space\Space\Resources\CameraDreapta.png");
+
+        public Form1()
+        {
+            InitializeComponent();
+            BackgroundImage = hub;
+        }
+
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.W)
+            {
+                Up.Enabled = true;
+            }
+            if (e.KeyCode == Keys.S)
+            {
+                Down.Enabled = true;
+            }
+            if (e.KeyCode == Keys.A)
+            {
+                Left.Enabled = true;
+            }
+            if (e.KeyCode == Keys.D)
+            {
+                Right.Enabled = true;
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.W)
+            {
+                Up.Enabled = false;
+            }
+            if (e.KeyCode == Keys.S)
+            {
+                Down.Enabled = false;
+            }
+            if (e.KeyCode == Keys.A)
+            {
+                Left.Enabled = false;
+            }
+            if (e.KeyCode == Keys.D)
+            {
+                Right.Enabled = false;
+            }
+        }
+
+        
+
+        private void Up_Tick(object sender, EventArgs e)
+        {
+            if (pictureBox1.Location.Y > 0)
+            {
+                newY = pictureBox1.Location.Y - pxlmvY;
+                pictureBox1.Location = new Point(pictureBox1.Location.X, newY);
+                pictureBox1.ImageLocation = pictureBoxArray[2];
+                if (roomUp == false)
+                    Up_Room_Entrance();
+                
+                if (roomDown == true)
+                    Down_Room_Exit();
+
+                if (roomUp || roomRight || roomLeft || roomDown)
+                {
+                    Interaction(pictureBox1, pictureBox2);
+                }
+            }
+            
+            
+        }
+
+        private void Down_Tick(object sender, EventArgs e)
+        {
+            if (pictureBox1.Location.Y < this.Height)
+            {
+                newY = pictureBox1.Location.Y + pxlmvY;
+                pictureBox1.Location = new Point(pictureBox1.Location.X, newY);
+                pictureBox1.ImageLocation = pictureBoxArray[3];
+                
+                if (roomUp == true)
+                    Up_Room_Exit();
+                if (roomDown == false)
+                    Down_Room_Entrance();
+
+                if (roomUp || roomRight || roomLeft || roomDown)
+                {
+                    Interaction(pictureBox1, pictureBox2);
+                }
+            }
+        }
+
+        private void Left_Tick(object sender, EventArgs e)
+        {
+            if (pictureBox1.Location.X > 0)
+            {
+                newX = pictureBox1.Location.X - pxlmvX;
+                pictureBox1.Location = new Point(newX, pictureBox1.Location.Y);
+                pictureBox1.ImageLocation = pictureBoxArray[1];
+                if (roomRight == true)
+                    Right_Room_Exit();
+                if (roomLeft == false)
+                    Left_Room_Entrance();
+
+                if (roomUp || roomRight || roomLeft || roomDown)
+                {
+                    Interaction(pictureBox1, pictureBox2);
+                }
+
+            }
+        }
+
+        private void Right_Tick(object sender, EventArgs e)
+        {
+            if (pictureBox1.Location.X < this.Width)
+            {
+                newX = pictureBox1.Location.X + pxlmvX;
+                pictureBox1.Location = new Point(newX, pictureBox1.Location.Y);
+                pictureBox1.ImageLocation = pictureBoxArray[0];
+                if (roomLeft == true)
+                    Left_Room_Exit();
+                if (roomRight == false)
+                    Right_Room_Entrance();
+
+                if (roomUp || roomRight || roomLeft || roomDown)
+                {
+                    Interaction(pictureBox1, pictureBox2);
+                }
+
+            }
+        }
+
+        public void Up_Room_Entrance()
+        {
+            if (pictureBox1.Location.Y == 0 && pictureBox1.Location.X >= 395 && pictureBox1.Location.X <= 525 && inRoom == false)
+            {
+                pictureBox1.Location = new Point(pictureBox1.Location.X, 1080);
+                BackgroundImage = rmup;
+                roomUp = true;
+                inRoom = true;
+                LoadUpRoom();
+               
+            }
+    
+        }
+
+        public void Up_Room_Exit()
+        {
+            if (pictureBox1.Location.Y == 1080 && pictureBox1.Location.X >= 395 && pictureBox1.Location.X <= 525)
+            {
+                pictureBox1.Location = new Point(pictureBox1.Location.X, 0);
+                BackgroundImage = hub;
+                roomUp = false;
+                inRoom = false;
+                HideUpRoom();
+            }
+        }
+
+        public void Down_Room_Entrance()
+        {
+            if (pictureBox1.Location.Y == 1080 && pictureBox1.Location.X >= 1180 && pictureBox1.Location.X <= 1300 && inRoom == false)
+            {
+                pictureBox1.Location = new Point(pictureBox1.Location.X, 0);
+                BackgroundImage = rmdown;
+                roomDown = true;
+                inRoom = true;
+            }
+
+        }
+        
+        public void Down_Room_Exit()
+        {
+            if (pictureBox1.Location.Y == 0 && pictureBox1.Location.X >= 1180 && pictureBox1.Location.X <= 1300)
+            {
+                pictureBox1.Location = new Point(pictureBox1.Location.X, 1080);
+                BackgroundImage = hub;
+                roomDown = false;
+                inRoom = false;
+            }
+        }
+
+
+        public void Left_Room_Entrance()
+        {
+            if (pictureBox1.Location.X == 0 && pictureBox1.Location.Y >= 490 && pictureBox1.Location.Y <= 595 && inRoom == false)
+            {
+                pictureBox1.Location = new Point(1920, pictureBox1.Location.Y);
+                BackgroundImage = rmleft;
+                roomLeft = true;
+                inRoom = true;
+            }
+
+        }
+
+        public void Left_Room_Exit()
+        {
+            if (pictureBox1.Location.X == 1920 && pictureBox1.Location.Y >= 490 && pictureBox1.Location.Y <= 595)
+            {
+                pictureBox1.Location = new Point(0, pictureBox1.Location.Y);
+                BackgroundImage = hub;
+                roomLeft = false;
+                inRoom = false;
+            }
+        }
+
+
+        public void Right_Room_Entrance()
+        {
+            if (pictureBox1.Location.X == 1920 && pictureBox1.Location.Y >= 470 && pictureBox1.Location.Y <= 565 && inRoom == false)
+            {
+                pictureBox1.Location = new Point(0, pictureBox1.Location.Y);
+                BackgroundImage = rmright;
+                roomRight = true;
+                inRoom = true;
+            }
+
+        }
+
+        public void Right_Room_Exit()
+        {
+            if (pictureBox1.Location.X == 0 && pictureBox1.Location.Y >= 470 && pictureBox1.Location.Y <= 565)
+            {
+                pictureBox1.Location = new Point(1920, pictureBox1.Location.Y);
+                BackgroundImage = hub;
+                roomRight = false;
+                inRoom = false;
+            }
+        }
+
+        private static double GetDistance(double x1, double y1, double x2, double y2)
+        {
+            return Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+        }
+
+        public void Interaction(PictureBox player, PictureBox obj)
+        {
+            double dist = GetDistance(player.Location.X, player.Location.Y, obj.Location.X, obj.Location.Y);
+            if (dist <= 170)
+                button1.Enabled = true;
+            else
+                button1.Enabled = false;
+        }
+
+        public void LoadUpRoom()
+        {
+            pictureBox2.Visible = true;
+        }
+
+        public void HideUpRoom()
+        {
+            pictureBox2.Visible = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
